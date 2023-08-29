182964f43fccae0d824251cd36e9b3278c60a745
Eddie Lin <eddielin0926@gmail.com>
2023-08-29 12:28:41 +0800

    ci: :green_heart: use poetry to publish

--- cjkfuzz/__init__.py
+++ cjkfuzz/__init__.py
@@ -2,6 +2,6 @@ from cjkfuzz import fuzz, hamming, levenshtein
 
 __author__: str = "Eddie Lin"
 __license__: str = "MIT"
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 __all__ = ["fuzz", "hamming", "levenshtein"]
