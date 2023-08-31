3ca3b7f3fe80cfd7df468a19717e3e7db34e761e
Eddie Lin <eddielin0926@gmail.com>
2023-08-31 23:57:26 +0800

    feat: :bookmark: release version 0.0.2

--- cjkfuzz/__init__.py
+++ cjkfuzz/__init__.py
@@ -2,6 +2,6 @@ from cjkfuzz import fuzz, hamming, jaro, jaro_winkler, levenshtein
 
 __author__: str = "Eddie Lin"
 __license__: str = "MIT"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __all__ = ["fuzz", "hamming", "jaro", "jaro_winkler", "levenshtein"]
