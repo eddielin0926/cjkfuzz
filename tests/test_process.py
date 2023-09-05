128b96bd263ce715c3fe909385f659a5e5195ac8
Eddie Lin <eddielin0926@gmail.com>
2023-09-05 17:11:20 +0800

    test: :white_check_mark: add fuzz process tokenizer tests

--- tests/test_process.py
+++ tests/test_process.py
@@ -1,3 +1,5 @@
+import pytest
+
 from cjkfuzz import process
 
 
@@ -5,3 +7,8 @@ def test_process_extract():
     choices = ["apple", "banana", "orange", "pear"]
     query = "banana"
     assert process.extract(query, choices, 2) == [(1.0, "banana"), (0.5833333333333334, "orange")]
+
+
+def test_process_extract_limit() -> None:
+    with pytest.raises(ValueError):
+        process.extract("中文測試", ["中文測試", "日本語テスト"], 0)
