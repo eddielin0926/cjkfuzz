0ec5080440d2b25f34d363189d1def5f0c5d7c52
Eddie Lin <eddielin0926@gmail.com>
2023-09-07 00:29:55 +0800

    feat: :sparkles: add scorer to process

--- cjkfuzz/process.py
+++ cjkfuzz/process.py
@@ -1,12 +1,15 @@
 """Provide a function to extract the best matches from a list of choices"""
 import bisect
-from typing import Hashable, Sequence, Tuple
+from typing import Callable, Hashable, Sequence, Tuple
 
-from cjkfuzz import fuzz
+from cjkfuzz import levenshtein
 
 
 def extract(
-    query: Sequence[Hashable], choices: Sequence[Sequence[Hashable]], limit: int = 1
+    query: Sequence[Hashable],
+    choices: Sequence[Sequence[Hashable]],
+    limit: int = 1,
+    scorer: Callable[..., float] = levenshtein.ratio,
 ) -> Tuple[Sequence[Hashable], int]:
     """Return a list of the best matches
 
@@ -17,6 +20,10 @@ def extract(
             The list of sequences to match against.
         limit:
             The maximum number of matches to return. The default value is 1.
+        scorer:
+            A function that provides a measure of the similarity between two sequences and it
+            should returns a float between 0 and 1, inclusive. The default scorer is the Levenshtein
+            ratio.
 
     Returns:
         A tuple of the best matches and the best match's score. The best matches are sorted in
@@ -29,9 +36,9 @@ def extract(
         raise ValueError("limit must be greater than 0")
     candidates = []
     for idx, choice in enumerate(choices):
-        ratio = fuzz.ratio(query, choice)
+        score = scorer(query, choice)
         # Keep the list sorted and limit the number of candidates
-        bisect.insort(candidates, (ratio, choice))
+        bisect.insort(candidates, (score, choice))
         if len(candidates) > limit:
             # Remove the worst candidate
             candidates.pop(0)
